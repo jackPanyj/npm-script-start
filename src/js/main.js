b13d65a0f8b83e163f3377e039adb4b15942a78a
jackpanyj <1016432619@qq.com>
2017-06-19 14:49:51 +0800

    done

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+  const doc = document.documentElement;
+  [1,2,3].map(v => v * v)
+  doc.classList.remove('no-js');
+  doc.classList.add('js');
---
+++ src/js/sub.js
@@ -0,0 +1,4 @@
+  if (!window.localStorage.getItem('hasVisited')) {
+    document.querySelector('.Tour').classList.add('is-active');
+    window.localStorage.setItem('hasVisited', 'true');
+  }
